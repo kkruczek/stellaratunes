8c8c6c1e4cd0bb8ffab6dd759121a1552a1443d0
Jurek Dörfler <jdorfler@pgs-soft.com>
2018-01-26 08:54:50 +0100

    After the workshop.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './styles.css';
 import logo from './logo.svg';
 import Button from '../Button/index';
 import Input from '../Input/index';
 
-function Header() {
+function Header({ query, onInputChange }) {
   return (
     <div className="header">
       <img className="logo" src={logo} alt="logo"/>
       <h1 className="title">Stellaratunes</h1>
-      <Input />
-      <Button type="primary">Search</Button>
+      <Input
+        type="text"
+        value={query}
+      />
+      <Button type="primary" onButtonClick={onInputChange}>Search</Button>
     </div>
   );
 }
+
+Header.propTypes = {
+  query: PropTypes.string,
+  onInputChange: PropTypes.func
+};
 
 export default Header;
